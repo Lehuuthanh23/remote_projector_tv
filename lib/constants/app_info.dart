3090c42832b130bc5f4975e7b39520af672496b1
Lehuuthanh23 <thanhlebtre1245@gmail.com>
2024-08-30 16:48:18 +0700

    sửa version

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -2,11 +2,11 @@ class AppInfo {
   AppInfo._();
 
   static const userAndroidAppInfo = BaseInfo(
-    version: 'U1.0.0.1',
+    version: 'TV1.0.0.1',
     buildDate: '30/08/2024 17:00',
   );
   static const userIOSAppInfo = BaseInfo(
-    version: 'U1.0.0.1',
+    version: 'TV1.0.0.1',
     buildDate: '30/08/2024 15:00',
   );
 }
